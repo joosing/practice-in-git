c76062ec9213c2b0b088a8eac9c766d19f82a2c3
Jsing <joosing711@gmail.com>
2023-07-13 20:47:54 +0900

    hello

---
+++ src/main/java/com/joosing/practiceingit/PracticeInGitApplication.java
@@ -0,0 +1,13 @@
+package com.joosing.practiceingit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeInGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeInGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joosing/practiceingit/PracticeInGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joosing.practiceingit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeInGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
